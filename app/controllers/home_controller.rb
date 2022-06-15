1b9eb5e20fbcc3b0609a35ad414cb1dca170867f
Andrew Panko <trikster.seo@gmail.com>
2022-06-15 14:24:19 +0300

    add post features

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   def index
     redirect_to new_user_session_path unless user_signed_in?
+
+    @posts = Post.order(created_at: :desc)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,15 @@
 class User < ApplicationRecord
     has_many :posts, dependent: :destroy
-end
+  end
 
 class Post < ApplicationRecord
     belongs_to :user
 
     has_one_attached :image
+
+    validate :image_presence
+    
+    def image_presence
+      errors.add(:image, "can't be blank") unless image.attached?
+    end
 end
