6ee2bddbf9e14f9c0abc358dfc836820b32f4dcb
Andrew Panko <trikster.seo@gmail.com>
2022-06-16 21:43:33 +0300

    change auth in controllers

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,8 @@
 class HomeController < ApplicationController
-  before_action :require_login
+  before_action :authenticate_user!
 
   def index   
     @posts = Post.order(created_at: :desc)
   end
 
-  private
-
-  def require_login
-    redirect_to new_user_session_path unless user_signed_in?
-  end
 end
