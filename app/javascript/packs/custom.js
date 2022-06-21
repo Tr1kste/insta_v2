adf777b5158dce302241ceae7385c756e2ff796f
Andrew Panko <trikster.seo@gmail.com>
2022-06-21 22:55:48 +0300

    edit js

--- app/javascript/packs/custom.js
+++ app/javascript/packs/custom.js
@@ -1,11 +1,9 @@
-window.onload = function () {
-  const el = document.querySelector("#file_input");
-  if (el) {
-    el.addEventListener("change", (event) => {
-      const img = document.querySelector(".newPost__leftColumn__img");
-      const p = document.querySelector(".newPost__leftColumn__p");
-      img.src = URL.createObjectURL(event.target.files[0]);
-      p.innerHTML = event.target.files[0].name;
-    });
-  }
-};
+const el = document.querySelector("#file_input");
+if (el) {
+  el.addEventListener("change", (event) => {
+    const img = document.querySelector(".newPost__leftColumn__img");
+    const p = document.querySelector(".newPost__leftColumn__p");
+    img.src = URL.createObjectURL(event.target.files[0]);
+    p.innerHTML = event.target.files[0].name;
+  });
+}
