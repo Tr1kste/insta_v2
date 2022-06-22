b5c1778858e5e68fa5acf4c368569c5d448af6c4
Andrew Panko <trikster.seo@gmail.com>
2022-06-22 16:37:04 +0300

    edit styles for new & edit post

--- app/javascript/packs/custom.js
+++ app/javascript/packs/custom.js
@@ -1,7 +1,7 @@
 const el = document.querySelector("#file_input");
 if (el) {
   el.addEventListener("change", (event) => {
-    const img = document.querySelector(".newPost__leftColumn__img");
+    const img = document.querySelector(".img_post");
     const p = document.querySelector(".newPost__leftColumn__p");
     img.src = URL.createObjectURL(event.target.files[0]);
     p.innerHTML = event.target.files[0].name;
