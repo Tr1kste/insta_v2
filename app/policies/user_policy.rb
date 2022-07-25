bd3b13d5c1b2a6ee64be5867df45d05d9e472af1
tr1kster <trikster.seo@gmail.com>
2022-07-25 21:09:19 +0300

    rubocop edit

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,16 +1,18 @@
+# frozen_string_literal: true
+
 class UserPolicy
-    attr_reader :user, :current_profile
-  
-    def initialize(user, current_profile)
-      @current_user = user
-      @current_profile = current_profile
-    end
-  
-    def edit?
-        update?
-    end
-  
-    def update?
-        @current_user == @current_profile
-    end
+  attr_reader :user, :current_profile
+
+  def initialize(user, current_profile)
+    @current_user = user
+    @current_profile = current_profile
+  end
+
+  def edit?
+    update?
+  end
+
+  def update?
+    @current_user == @current_profile
+  end
 end
